4e182ff0011762c59cfec30ee52e94207cfde507
yzleaf <yezulong@163.com>
2021-01-08 12:29:12 -0500

    binary search finish

---
+++ binarysearch/BinarySearchOther.java
@@ -0,0 +1,249 @@
+package binarysearch;
+
+public class BinarySearchOther {
+
+    // 1. Search a 2D Matrix
+    /**
+     * @param matrix, a list of lists of integers
+     * @param target, an integer
+     * @return a boolean, indicate whether matrix contains target
+     */
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0) {
+            return false;
+        }
+        if (matrix[0] == null || matrix[0].length == 0) {
+            return false;
+        }
+
+        int row = matrix.length, column = matrix[0].length;
+        int start = 0, end = row * column - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            int number = matrix[mid / column][mid % column];
+            if (number == target) {
+                return true;
+            } else if (number > target) {
+                end = mid;
+            } else { // number < target
+                start = mid;
+            }
+        }
+
+        if (matrix[start / column][start % column] == target) {
+            return true;
+        }
+        if (matrix[end / column][end % column] == target) {
+            return true;
+        }
+
+        return false;
+    }
+
+    // 2. Search a 2D Matrix II
+    /**
+     * @param matrix: A list of lists of integers
+     * @param target: number you want to search in the matrix
+     * @return An integer indicate the occurrence of target in the given matrix
+     */
+    public int searchMatrixII(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0) {
+            return 0;
+        }
+        if (matrix[0] == null || matrix[0].length == 0) {
+            return 0;
+        }
+
+        int row = matrix.length;
+        int column = matrix[0].length;
+        int x = row - 1, y = 0; // 从左下角开始找
+        int count = 0;
+
+        while (x >= 0 && y < column) {
+            if (matrix[x][y] < target) { // 小于target，向右找（增大当前值）
+                y++;
+            } else if (matrix[x][y] > target) { // 大于target，向上找（减小当前值）
+                x--;
+            } else { // 相等
+                count++;
+                x--;
+                y++;
+            }
+        }
+        return count;
+    }
+
+    // 3. Search for a Range
+    /**
+     * @param A: an integer sorted array
+     * @param target: an integer to be inserted
+     * @return a list of length 2, [index1, index2]
+     */
+    public int[] searchRange(int[] A, int target) {
+        if (A.length == 0) {
+            return new int[]{-1, -1};
+        }
+
+        int start, end, mid;
+        int[] bound = new int[2];
+
+        // left bound (1st occurrence)
+        start = 0;
+        end = A.length - 1;
+        while (start + 1 < end) {
+            mid = start + (end - start) / 2;
+            if (A[mid] == target) {
+                end = mid;
+            } else if (A[mid] < target) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+        if (A[start] == target) {
+            bound[0] = start;
+        } else if (A[end] == target) {
+            bound[0] = end;
+        } else { // not find
+            bound[0] = bound[1] = -1;
+            return bound;
+        }
+
+        // right bound (last occurrence)
+        start = 0;
+        end = A.length - 1;
+        while (start + 1 < end) {
+            mid = start + (end - start) / 2;
+            if (A[mid] == target) {
+                start = mid;
+            } else if (A[mid] < target) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+        if (A[end] == target) {
+            bound[1] = start;
+        } else if (A[start] == target) {
+            bound[1] = end;
+        } else {
+            bound[0] = bound[1] = -1;
+            return bound;
+        }
+
+        return bound;
+    }
+
+    // 4. Total Occurrence of Target
+    /**
+     * @param A: A an integer array sorted in ascending order
+     * @param target: An integer
+     * @return An integer
+     */
+    // Same as Q3
+    // return bound[1] - bound[0] + 1;
+    public int totalOccurrence(int[] A, int target) {
+        return -1;
+    }
+
+    // 5. Smallest Rectangle Enclosing Black Pixels
+    /**
+     * @param image a binary matrix with '0' and '1'
+     * @param x, y the location of one of the black pixels
+     * @return an integer
+     */
+    // 找到一个可以包含所有1的矩形
+    public int minArea(char[][] image, int x, int y) {
+        if (image == null || image.length == 0 || image[0].length == 0) {
+            return 0;
+        }
+
+        int row = image.length;
+        int column = image[0].length;
+
+        int left = findLeft(image, 0, y); // 找到最左边出现1的列
+        int right = findRight(image, y, column - 1); // 找到最右边出现1的列
+        int top = findTop(image, 0, x); // 找到最上面出现1的行
+        int bottom = findBottom(image, x, row - 1); // 找到最下面出现1的行
+
+        return (right - left + 1) * (top - bottom + 1);
+    }
+    private int findLeft(char[][] image, int start, int end) {
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (isEmptyColumn(image, mid)) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+        if (isEmptyColumn(image, start)) {
+            return end;
+        }
+
+        return start;
+    }
+    private int findRight(char[][] image, int start, int end) {
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (isEmptyColumn(image, mid)) {
+                end = mid;
+            } else {
+                start = mid;
+            }
+        }
+        if (isEmptyColumn(image, end)) {
+            return start;
+        }
+
+        return end;
+    }
+    private int findTop(char[][] image, int start, int end) {
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (isEmptyRow(image, mid)) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+        if (isEmptyRow(image, start)) {
+            return end;
+        }
+
+        return start;
+    }
+    private int findBottom(char[][] image, int start, int end) {
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (isEmptyRow(image, mid)) {
+                end = mid;
+            } else {
+                start = mid;
+            }
+        }
+        if (isEmptyRow(image, end)) {
+            return start;
+        }
+
+        return end;
+    }
+
+    private boolean isEmptyColumn(char[][] image, int col) {
+        for (int i = 0; i < image.length; i++) {
+            if (image[i][col] == '1') {
+                return false;
+            }
+        }
+        return true;
+    }
+    private boolean isEmptyRow(char[][] image, int row) {
+        for (int j = 0; j < image[0].length; j++) {
+            if (image[row][j] == '1') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
--- binarysearch/BinarySearchPosition.java
+++ binarysearch/BinarySearchPosition.java
@@ -1,5 +1,7 @@
 package binarysearch;
 
+import com.sun.xml.internal.fastinfoset.tools.XML_SAX_StAX_FI;
+
 public class BinarySearchPosition {
 
     // 1. find bad version
@@ -87,7 +89,8 @@ public class BinarySearchPosition {
         int start = 0, end = nums.length - 1;
         while (start + 1 < end) {
             int mid = start + (end - start) / 2;
-            if (nums[mid] > nums[end]) {
+            // 如果找的是数组内的最大值，则和nums[start]比较
+            if (nums[mid] > nums[end]) { // 不用[start]比较，因为如果数组不rotated，start=mid会直接覆盖
                 start = mid;
             } else {
                 end = mid;
@@ -96,4 +99,99 @@ public class BinarySearchPosition {
 
         return Math.min(nums[start], nums[end]);
     }
+
+
+    // 4. Maximum Number in Mountain Sequence
+    /**
+     * @param nums a mountain sequence which increase firstly and then decrease
+     * @return then mountain top
+     */
+    public int mountainSequence(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+
+        int start = 0, end = nums.length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] > nums[mid + 1]) {
+                end = mid;
+            } else { // nums[mid] <= nums[mid+1]
+                start = mid;
+            }
+        }
+
+        return Math.max(nums[start],nums[end]);
+    }
+
+
+    // 5. Find Peak Element
+    /**
+     * @param A: An integers array.
+     * @return return any of peek positions.
+     */
+    // A[0] < A[1] && A[n-2] > A[n-1]
+    public int findPeak(int[] A) {
+        int start = 0, end = A.length - 1 - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (A[mid] < A[mid - 1]) { // 下降沿
+                end = mid;
+            } else if (A[mid] < A[mid + 1]) { // 上升沿
+                start = mid;
+            } else { // A[mid]>A[mid-1] && A[mid]>A[mid+1] -> Peak
+                return mid;
+            }
+        }
+
+        if (A[start] > A[end]) {
+            return start;
+        } else {
+            return end;
+        }
+    }
+
+    // 6. Search in Rotated Sorted Array
+    // 方法1 先找到最小点，再来一次二分
+    // 方法2 直接二分分类讨论
+    public int search(int[] A, int target) {
+
+        if (A == null || A.length == 0) {
+            return -1;
+        }
+
+        int start = 0, end = A.length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (A[mid] == target) {
+                return mid;
+            }
+
+            if (A[start] < A[mid]) { // left part
+                if (A[start] <= target && target <= A[mid]) {
+                    end = mid;
+                } else { // 在left part, start不可能大于target
+                         // 只有target > A[mid]
+                    start = mid;
+                }
+            } else { // right part
+                if (A[mid] <= target && target <= A[end]) {
+                    start = mid;
+                } else { // 在right part, end不可能小于target
+                         // 只有target < A[mid]
+                    end = mid;
+                }
+            }
+        } // while
+
+        if (A[start] == target) {
+            return start;
+        }
+        if (A[end] == target) {
+            return end;
+        }
+
+        return -1;
+    }
+
 }
