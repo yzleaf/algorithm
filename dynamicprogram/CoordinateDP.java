510f67cde964ffdc3b94042d71fe83e81d1b6c35
yzleaf <yezulong@163.com>
2021-01-31 01:13:20 -0500

    fantan DP

--- dynamicprogram/CoordinateDP.java
+++ dynamicprogram/CoordinateDP.java
@@ -104,4 +104,105 @@ public class CoordinateDP {
             return (int)(numer / denomi);
         }
     }
+
+    // 3. Climbing Stairs
+    // 假设你正在爬楼梯，需要n步你才能到达顶部。但每次你只能爬一步或者两步，你能有多少种不同的方法爬到楼顶部
+    // 输入:  n= 3
+    // 输出: 3
+    // 解释：
+    //    1) 1, 1, 1
+    //    2) 1, 2
+    //    3) 2, 1
+    public class ClimbStairsSolution {
+        // 考虑最后一步走1阶还是走2阶。
+        // 方案数Dpn = 最后一步走1阶的方案数 + 最后一步走2阶的方案数。
+        // Dpn = Dpn-1 + Dpn-2.
+        public int climbStairs(int n) {
+            if (n <= 1) {
+                return n;
+            }
+            int last = 1; // 最后一步走一阶，走到当前点时之前所有的方案数（初始为1）
+            int lastLast = 1; // 最后一步走两阶，走到当前点时之前所有的方案数（初始为1）
+            int now = 0; // 从2开始一直到n
+
+            for (int i = 2; i <= n; i++) {
+                now = last + lastLast;
+                lastLast = last;
+                last = now;
+            }
+            return now;
+        }
+    }
+
+    // 4. Jump Game
+    // 给出一个非负整数数组，你最初定位在数组的第一个位置。　　　
+    // 数组中的每个元素代表你在那个位置可以跳跃的最大长度。　　　　
+    // 判断你是否能到达数组的最后一个位置。
+    public class JumpGameSolution {
+        public boolean canJump(int[] A) {
+            boolean[] can = new boolean[A.length]; // 能否跳到当前位置
+            can[0] = true;
+
+            for (int i = 1; i < A.length; i++) { // 能否到达i位置
+                for (int j = 0; j < i; j++) { // 能否从j跳到i
+                    if (can[j] && j + A[j] >= i) { // 从j位置+可以跳跃的长度大于等于i位置
+                        can[i] = true;
+                        break;
+                    }
+                }
+            }
+
+            return can[A.length - 1];
+        }
+        // 优化空间复杂度
+        // 如果可以跳到i点，则说明一定可以跳到i前面的任意一点。所以，如果i位为True，前面的位置一定是True
+        public boolean canJump2(int[] A) {
+            boolean dp = true; // 是否能够到达当前位置
+                               // 第一个点默认能到达
+
+            for (int i = 1; i < A.length; i++) { // 能否到达i位置
+                for (int j = 0; j < i; j++) { // 能否从j跳到i
+                    if (j + A[j] >= i) { // 从j位置+可以跳跃的长度大于等于i位置
+                        dp = true;
+                        break;
+                    } else {
+                        dp = false;
+                    }
+                }
+                if (dp == false) { // 无法到达当前i点，直接返回
+                    return false;
+                }
+            }
+
+            return dp;
+        }
+    }
+
+    // 5. Jump Game II
+    // 给出一个非负整数数组，你最初定位在数组的第一个位置
+    // 数组中的每个元素代表你在那个位置可以跳跃的最大长度
+    // 你的目标是使用最少的跳跃次数到达数组的最后一个位置
+    public class JumpGame2Solution {
+
+        public int jump(int[] A) {
+            // steps[i]表示跳到下标为i的位置最少需要几次跳跃
+            int[] steps = new int[A.length];
+
+            steps[0] = 0;
+            for (int i = 1; i < A.length; i++) {
+                steps[i] = Integer.MAX_VALUE;
+            }
+
+            for (int i = 1; i < A.length; i++) {
+                for (int j = 0; j < i; j++) {
+                    if (steps[j] != Integer.MAX_VALUE && j + A[j] >= i) { // 能从j到达i
+                        steps[i] = Math.min(steps[j] + 1, steps[i]); // 从j跳过来需要+1，之前遍历已经记录了一个steps[i]需要比较
+                                                                     // 每个step[i]都能记录下到达当前点的最小跳跃次数
+                    }
+                }
+            }
+            return steps[A.length - 1];
+        }
+    }
+
 }
---
+++ dynamicprogram/FanTanDP.java
@@ -0,0 +1,245 @@
+package dynamicprogram;
+
+import java.util.*;
+
+public class FanTanDP {
+    // 1. Longest Increasing Subsequence
+    // 300
+    // 给定一个整数序列，找到最长上升子序列（LIS），返回LIS的长度
+    // sequence是可以不连续的
+    public class LongestIncreasingSubsequenceSolution {
+        public int longestIncreasingSubsequence(int[] nums) {
+            if (nums == null || nums.length == 0) {
+                return 0;
+            }
+
+            int n = nums.length;
+
+            // 设 dp[i] 表示以 nums[i] 为结尾的最长上升子序列的长度
+            // 为了保证元素单调递增，只能从i前面且末尾元素比nums[i]小的状态转移过来
+            int[] dp = new int[n];
+
+            // 初始化 以自己结尾 默认为1
+            for (int i = 0; i < n; i++) {
+                dp[i] = 1;
+            }
+
+            // 判断状态
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < i; j++) { // 从A[j]到A[i]是否上升，且记录到i位置的最大增序列长度
+                    if (nums[j] < nums[i]) {
+                        dp[i] = Math.max(dp[j] + 1, dp[i]);
+                    }
+                }
+            }
+
+            int max = 0;
+            for (int i = 0; i < n; i++) {
+                max = Math.max(max, dp[i]);
+            }
+            return max;
+        }
+
+        // 方法2 贪心+二分
+        // 建立dp数组，下标表示最长子序列长度，值表示当前这个子序列长度的最小结尾数据
+        // 从前往后遍历nums数组，更改dp数组的值
+        //    如果nums大于dp数组当前下标对应的值，直接更新至dp数组的下一位（最长子序列+1，新数值进入）
+        //    如果nums小于等于dp数组当前下标对应的值，利用二分查找在当前下标之前找到nums应该在的位置并更新至nums值（更新最小结尾数）
+        // 返回dp最大的有效index
+        public int longestIncreasingSubsequence2(int[] nums) {
+            int[] minLast = new int[nums.length + 1]; // 当前长度下最小的尾部元素
+            minLast[0] = Integer.MIN_VALUE;
+            for (int i = 1; i <= nums.length; i++) {
+                minLast[i] = Integer.MAX_VALUE;
+            }
+
+            for (int i = 0; i < nums.length; i++) {
+                // find the first number in minLast >= nums[i]
+                // 如果nums[i]大于现有元素，会在后面一位直接添加
+                // 如果nums[i]小于现有元素，会到前面去找，并且替换
+                int index = binarySearch(minLast, nums[i]);
+                minLast[index] = nums[i];
+            }
+
+            // 返回最长子序列结果
+            for (int i = nums.length; i >= 1; i--) {
+                if (minLast[i] != Integer.MAX_VALUE) {
+                    return i;
+                }
+            }
+            return 0;
+        }
+        // find the first number >= num 找第一个大于等于的元素替换它
+        private int binarySearch(int[] minLast, int num) {
+            int start = 0, end = minLast.length - 1;
+            while (start + 1 < end) {
+                int mid = start + (end - start) / 2;
+                if (minLast[mid] < num) {
+                    start = mid;
+                } else {
+                    end = mid;
+                }
+            }
+
+            // 因为nums数组是n+1的，所以start从无穷小开始，所以start踩的位置永远都小于num，这个if其实是不起作用的
+            // 放在这是为了跟模板一致
+            if (minLast[start] == num) { // 先找左边的
+                return start;
+            }
+
+            return end;
+        }
+
+    }
+
+    // 2. Russian Doll Envelopes
+    // 354
+    // 给一定数量的信封，带有整数对 (w, h) 分别代表信封宽度和高度。一个信封的宽高均大于另一个信封时可以放下另一个信封。
+    // 求最大的信封嵌套层数
+    public class MaxEnvelopesSolution {
+        // 先按宽度排序，再对高度求最长子序列
+        public int maxEnvelopes(int[][] envelopes) {
+            int n = envelopes.length;
+            // 按宽度升序排列
+            // 如果宽度一样，则按高度降序排列（因为如果高度升序的话，会把相同宽度的信封这个数组也算作一个增加的序列，但实际上宽度相同是不能嵌套的）
+            Arrays.sort(envelopes, (a, b) -> a[0] != b[0] ? a[0] - b[0]
+                                                          : b[1] - a[0]);
+
+            // 对高度数组寻找 LIS
+            int[] height = new int[n];
+            for (int i = 0; i < n; i++) {
+                height[i] = envelopes[i][1];
+            }
+
+            return lengthOfLIS(height);
+        }
+        // 求最长子序列，见上一题
+        private int lengthOfLIS(int[] nums) {
+            return 0;
+        }
+    }
+
+    // 3. 最大整除子集 · Largest Divisible Subset
+    // 给一个由 无重复的正整数 组成的集合
+    // 找出满足任意两个元素 (Si, Sj) 都有 Si % Sj = 0 或 Sj % Si = 0 成立的最大子集
+    public class LargestDivisibleSubsetSolution {
+        // 1、对数组进行自然排序，找到某一时刻的状态
+        // 2、状态分析（要找当前点的最大整数子集）
+        //     第一次：取到1：1前面没有数，自己组成一个整除子集 [1]
+        //     第二次：取到2：拿2跟之前的数进行对比，会有三种情况，以1为例
+        //            如果2是1的倍数，那么把2放入1的最大整除子集，成为2的最大整除子集
+        //            如果2不是1的倍数，那么2再往前比
+        //            如果2比完前面所有的数都没有发现最大整除子集，那么把2自己作为一个整除子集
+        //            很明显2的最大整除子集是 [1,2]
+        //     第三次：取到3：拿2跟之前的数进行对比，会有三种情况，与第二次逻辑一致，
+        //            结果是[1,3]
+
+        public List<Integer> largestDivisibleSubset(int[] nums) {
+            int[] lengths = new int[nums.length]; // 当前数加入后最大整除子集的长度
+            int[] lasts = new int[nums.length]; // 当前数加入的最大整除子集的上一个数坐标，为了输出这个子集用
+
+            Arrays.sort(nums);
+            for (int i = 0; i < nums.length; i++) {
+                int curLength = -1;
+                int curLastIndex = -1;
+
+                // 普通方法
+                for (int j = i - 1; j >= 0; j--) { // 不断往前比，如果发现能找到被当前数整除的数，且长度最长，记录上一个数的坐标跟长度
+                    if (nums[i] % nums[j] == 0 && lengths[j] + 1 > curLength) { // 可以整除j这个数且最大长度更长
+                        curLength = lengths[j] + 1;
+                        curLastIndex = j; // 记录上一个数
+                    }
+                }
+                // 如果找不到，就设置为1，把自己作为这个整除子集
+                lengths[i] = curLength == -1 ? 1 : curLength;
+                lasts[i] = curLastIndex == -1 ? i : curLastIndex;
+            }
+
+            int maxLength = -1;
+            int index = -1;
+            // 找哪个数具有最大长度的整除子集
+            for (int i = 0; i < lengths.length; i++) {
+                if (lengths[i] > maxLength) {
+                    maxLength = lengths[i];
+                    index = i;
+                }
+            }
+
+            List<Integer> result = new ArrayList<>();
+            for (int i = 0; i < maxLength; i++) {
+                result.add(nums[index]);
+                index = lasts[index];
+            }
+
+            return result;
+        }
+
+        public List<Integer> largestDivisibleSubset2(int[] nums) {
+            int[] lengths = new int[nums.length]; // 当前数加入后最大整除子集的长度
+            int[] lasts = new int[nums.length]; // 当前数加入的最大整除子集的上一个数坐标，为了输出这个子集用
+
+            Arrays.sort(nums);
+            Map<Integer, Integer> valueToIndex = new HashMap<>(); // 存储排好序的nums数组里的value和index
+            for (int i = 0; i < nums.length; i++) {
+                valueToIndex.put(nums[i], i);
+            }
+
+            for (int i = 0; i < nums.length; i++) {
+                int curLength = -1;
+                int curLastIndex = -1;
+
+                // 优化方法，找这个数的因子
+                for (Integer factor : getFactors(nums[i])) {
+                    if (valueToIndex.containsKey(factor)) {
+                        int factorIndex = valueToIndex.get(factor);
+                        if (lengths[factorIndex] + 1 > curLength) { // 可以整除j这个数且最大长度更长
+                            curLength = lengths[factorIndex] + 1;
+                            curLastIndex = factorIndex; // 记录上一个数
+                        }
+                    }
+                }
+
+                // 如果找不到，就设置为1，把自己作为这个整除子集
+                lengths[i] = curLength == -1 ? 1 : curLength;
+                lasts[i] = curLastIndex == -1 ? i : curLastIndex;
+            }
+
+            int maxLength = -1;
+            int index = -1;
+            // 找哪个数具有最大长度的整除子集
+            for (int i = 0; i < lengths.length; i++) {
+                if (lengths[i] > maxLength) {
+                    maxLength = lengths[i];
+                    index = i;
+                }
+            }
+
+            List<Integer> result = new ArrayList<>();
+            for (int i = 0; i < maxLength; i++) {
+                result.add(nums[index]);
+                index = lasts[index];
+            }
+
+            return result;
+        }
+
+        private List<Integer> getFactors(int num) { // 获得因子
+            List<Integer> factors = new ArrayList();
+            if (num == 1) {
+                return factors;
+            }
+            int factor = 1;
+            while (factor * factor <= num) { // sqrt n的时间
+                if (num % factor == 0) {
+                    factors.add(factor);
+                    if (factor != 1 && num / factor != factor) { // 可理解成对称方向的factor，这里必须往里面加这个数才可以让while的条件成立
+                        factors.add(num / factor);
+                    }
+                }
+                factor++;
+            }
+            return factors;
+        }
+    }
+
+}
