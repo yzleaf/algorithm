8ef9fc7230d13479ddaf975c425c46c7c1657ee8
Zulong Ye <yzl@10-18-82-239.dynapool.wireless.nyu.edu>
2021-11-03 17:10:58 -0400

    diagnonal traverse I and II

--- math/Matrix.java
+++ math/Matrix.java
@@ -1,5 +1,7 @@
 package math;
 
+import  java.util.*;
+
 public class Matrix {
     // 1. Rotate Image
     // 给定一个N×N的二维矩阵表示图像，90度顺时针旋转图像。
@@ -98,5 +100,83 @@ public class Matrix {
 
         // 再改进，把B矩阵中0元素拿走，用List记录非0元素的行列坐标
         // 这样第三重for循环只要去加B的非零元素
+    }
+
+    // 498. Diagonal Traverse
+    // 按对角线顺序输出元素，第一次往斜向上，第二次就要斜向下
+    public class FindDiagnosalSolution {
+        public int[] findDiagonalOrder(int[][] mat) {
+
+            // 对角线上x和y的坐标和是相等的
+            // 如果斜向上，x横坐标增大，y纵坐标减小
+            // 如果斜向下，x纵坐标减小，y横坐标增大
+            int m = mat.length;
+            int n = mat[0].length;
+
+            int res[] = new int[m * n];
+            int index = 0;
+            boolean flag = true; // 斜向上/斜向下
+
+            // i是对角线的元素和
+            for (int i = 0; i < m + n - 1; i ++) {
+                int r = 0, c = 0;
+                if (flag) {
+                    r = (i < m) ? i : m - 1; // x要从最大的开始（因为斜向右上输出）
+                    c = i - r;
+                    while (r >= 0 && c < n) {
+                        res[index] = mat[r][c];
+                        index ++;
+                        r --;
+                        c ++;
+                    }
+                } else {
+                    c = (i < n) ? i : n - 1; // y要从最大的开始（因为斜向左下输出）
+                    r = i - c;
+                    while (r < m && c >= 0) {
+                        res[index] = mat[r][c];
+                        index ++;
+                        r ++;
+                        c --;
+                    }
+                }
+                flag = !flag;
+            }
+
+            return res;
+        }
+        // 方法2 遍历数组，用HashMap存(key:对角线左边和，val:具体数字)
+        // 然后再按key的值输出
+    }
+
+    // 1424. Diagonal Traverse II
+    // 按对角线输出元素，斜向下，但是是稀疏矩阵
+    // 要用HashMap来存：每个对角线坐标和key --- 对应的元素数值
+    public class FindDiagnorlIISolution {
+        public int[] findDiagonalOrder(List<List<Integer>> nums) {
+
+            List<List<Integer>> mHash = new ArrayList<>();
+            int num = 0; // 记录所有元素个数，方便最后建int数组的大小
+            // 遍历整个矩阵数组，把不同元素存入不同的坐标和下
+            for (int i = 0; i < nums.size(); i ++) {
+                for (int j = 0; j < nums.get(i).size(); j ++) {
+                    if (mHash.size() <= i + j) {
+                        mHash.add(new ArrayList<>());
+                    }
+                    mHash.get(i + j).add(nums.get(i).get(j));
+                    num ++;
+                }
+            }
+
+            int[] res = new int[num];
+            int index = 0;
+            for (int i = 0; i < mHash.size(); i ++) {
+                for (int j = mHash.get(i).size() - 1; j >= 0; j --) {
+                    res[index] = mHash.get(i).get(j);
+                    index ++;
+                }
+            }
+
+            return res;
+        }
     }
 }
