a78aff0787f3acc7f3e27fabe8f115cc66eca859
Zulong Ye <yzl@10-18-82-239.dynapool.wireless.nyu.edu>
2021-11-06 11:01:20 -0400

    sort matrix search ele

--- heap/Heap.java
+++ heap/Heap.java
@@ -489,6 +489,51 @@ public class Heap {
         // 方法2  二分
         // 起始数区间是[0][0] - [n][m]，二分查找中间的数mid排多少位
         // 从左下角开始，目标值更大->往右找，目标值更小->往上找
+        // 每次查看小于中间数mid的个数有多少
+        //    如果个数k<mid，mid应该右移，以获得更多的小于它的数(start = mid)
+        //    如果个数k>mid，mid应该左移，以获得更少的小于它的数(end = mid)
+        public int kthSmallest2(int[][] matrix, int k) {
+            int m = matrix.length;
+            int n = matrix[0].length;
+            int start = matrix[0][0];
+            int end = matrix[m - 1][n - 1];
+
+            while (start < end) {
+                // 每次循环都保证第K小的数在start~end之间，当start==end，第k小的数就是start
+                int mid = start + (end - start) / 2;
+                // 找二维矩阵中<mid的元素总个数
+                int count = findNotBiggerThanMid(matrix, mid, m, n);
+                if (count < k) {
+                    // 第k小的数在右半部分，且不包含mid
+                    start = mid + 1;
+                } else if (count > k) {
+                    // 第k小的数在左半部分，可能包含mid（下面的=）
+                    end = mid;
+                } else {
+                    end = mid;
+                }
+            }
+            return end;
+        }
+
+        private int findNotBiggerThanMid(int[][] matrix, int mid, int row, int col) {
+            // 以列为单位找，找到每一列最后一个<=mid的数即知道每一列有多少个数<=mid
+            // 从左下角开始找
+            int i = row - 1;
+            int j = 0;
+            int count = 0;
+            while (i >= 0 && j < col) {
+                if (matrix[i][j] <= mid) {
+                    // 第j列有i+1个元素<=mid
+                    count += i + 1;
+                    j ++; // 向右移动
+                } else {
+                    // 第j列目前的数大于mid，需要继续在当前列往上找
+                    i --;
+                }
+            }
+            return count;
+        }
     }
 
     // 239. 滑动窗口最大值
